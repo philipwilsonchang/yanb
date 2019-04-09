358d86eb01326125681e5d994885989847c1e376
Phillip Chang <pchang@novetta.com>
2019-04-09 17:04:20 +0000

    Implemented sql helper functions

--- db/sql.go
+++ db/sql.go
@@ -1,11 +1,11 @@
-package main
+package dbfuncs
 
 import (
 	"database/sql"
 	"time"
 
 	uuid "github.com/google/uuid"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3" // driver
 )
 
 var (
@@ -24,68 +24,181 @@ func Start(filepath string) error {
 
 // GetAllTransactions returns the entire table
 func GetAllTransactions() ([][]string, error) {
-	var (
-		results [][]string
-		//id         uuid.UUID
-		//cardid     string
-		//chargeTime time.Time
-		//vendor     string
-		//value      float64
-		//category   string
-	)
-	rows, err := db.Query("SELECT * FROM transactions")
+	var results [][]string
+
+	rows, err := db.Query("SELECT id, cardid, chargetime, vendor, value, category FROM transactions")
 	if err != nil {
 		return nil, err
 	}
 
-	rawResult := make([]string, 6)
-
 	for rows.Next() {
-		err = rows.Scan(rawResult)
+		resultPtrs := make([]interface{}, 6)
+		result := make([]string, 6)
+		for i := range result {
+			resultPtrs[i] = &result[i]
+		}
+		err = rows.Scan(resultPtrs...)
 		if err != nil {
 			return nil, err
 		}
-		results = append(results, rawResult)
+		results = append(results, result)
 	}
+
 	return results, nil
 }
 
 // GetAllDanglingTransactions gets all uncategorized transactions
 func GetAllDanglingTransactions() ([][]string, error) {
-	return nil, nil
+	var results [][]string
+
+	rows, err := db.Query("SELECT id, cardid, chargetime, vendor, value, category FROM transactions WHERE category = 'uncategorized'")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		resultPtrs := make([]interface{}, 6)
+		result := make([]string, 6)
+		for i := range result {
+			resultPtrs[i] = &result[i]
+		}
+		err = rows.Scan(resultPtrs...)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
 }
 
 // GetAllCategoryTransactions gets all categorized transactions
 func GetAllCategoryTransactions(category string) ([][]string, error) {
-	return nil, nil
+	var results [][]string
+
+	rows, err := db.Query("SELECT id, cardid, chargetime, vendor, value, category FROM transactions WHERE category = ?", category)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		resultPtrs := make([]interface{}, 6)
+		result := make([]string, 6)
+		for i := range result {
+			resultPtrs[i] = &result[i]
+		}
+		err = rows.Scan(resultPtrs...)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
 }
 
 // GetAllTransactionsBetween gets all transactions that occur between startTime and endTime
 func GetAllTransactionsBetween(startTime time.Time, endTime time.Time) ([][]string, error) {
-	return nil, nil
+	var results [][]string
+
+	rows, err := db.Query("SELECT id, cardid, chargetime, vendor, value, category FROM transactions WHERE chargetime > ? AND chargetime <= ?", startTime, endTime)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		resultPtrs := make([]interface{}, 6)
+		result := make([]string, 6)
+		for i := range result {
+			resultPtrs[i] = &result[i]
+		}
+		err = rows.Scan(resultPtrs...)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
 }
 
 // AddTransaction inserts a new transaction into the table
 func AddTransaction(id uuid.UUID, cardid string, chargeTime time.Time, vendor string, value float64, category string) error {
+	statement, err := db.Prepare("INSERT INTO transactions (id, cardid, chargetime, vendor, value, category) VALUES (?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec(id, cardid, chargeTime, vendor, value, category)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // ModifyTransaction modifies an existing transaction in the table by uuid
 func ModifyTransaction(id uuid.UUID, cardid string, chargeTime time.Time, vendor string, value float64, category string) error {
+	statement, err := db.Prepare("UPDATE transactions SET cardid = ?, chargetime = ?, vendor = ?, value = ?, category = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec(cardid, chargeTime, vendor, value, category, id)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // GetAllBudgetCategories spits out the entire budget table
 func GetAllBudgetCategories() ([][]string, error) {
-	return nil, nil
+	var results [][]string
+
+	rows, err := db.Query("SELECT category, budgetlimit, timeframe FROM budgets")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		resultPtrs := make([]interface{}, 3)
+		result := make([]string, 3)
+		for i := range result {
+			resultPtrs[i] = &result[i]
+		}
+		err = rows.Scan(resultPtrs...)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
 }
 
 // AddBudgetCategory adds a new category into the budget table
 func AddBudgetCategory(category string, limit float64, timeframe string) error {
+	statement, err := db.Prepare("INSERT INTO budgets (category, budgetlimit, timeframe) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec(category, limit, timeframe)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // ModifyBudgetCategory modifies an existing budget category in the budget table by category
 func ModifyBudgetCategory(category string, limit float64, timeframe string) error {
+	statement, err := db.Prepare("UPDATE budgets SET budgetlimit = ?, timeframe = ? WHERE category = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec(limit, timeframe, category)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- server/server.go
+++ server/server.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	//"github.com/gin-gonic/gin"
+	uuid "github.com/google/uuid"
+	sql "github.com/philipwilsonchang/budgetapp/db"
 	"net/http"
-	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world. %s", r.URL.Path[1:])
 }
 
+func dbtest(write bool) {
+	// Setup db
+	fmt.Println("Opening db file...")
+	sql.Start("testdb/test.db")
+
+	if write {
+		fmt.Println("Adding transactions...")
+		err := sql.AddTransaction(uuid.New(), "chase", time.Now(), "google", 16.05, "grocery")
+		if err != nil {
+			fmt.Printf("Error: %+v\n", err)
+		}
+
+		err = sql.AddTransaction(uuid.New(), "discover", time.Now(), "wholefoods", 17.99, "food")
+		if err != nil {
+			fmt.Printf("Error: %+v\n", err)
+		}
+	}
+
+	fmt.Println("Dumping tables...")
+	results, err := sql.GetAllTransactions()
+	if err != nil {
+		fmt.Printf("Error: %+v\n", err)
+	} else {
+		fmt.Printf("Dump: %+v\n", results)
+	}
+}
+
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	dbtest(false)
+	/*
+		router := gin.Default()
+		router.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
 		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+		router.Run() // listen and serve on 0.0.0.0:8080
+	*/
 }
