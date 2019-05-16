56445a488b61dbc9c5aaf04b3bcebcc37ecaf5c9
Phillip Chang <pchang@novetta.com>
2019-05-16 18:26:19 +0000

    Added routing structure

--- server/server.go
+++ server/server.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"github.com/gin-gonic/gin"
-	sql "github.com/philipwilsonchang/budgetapp/db"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
@@ -12,14 +11,62 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	sql.DBtest(false)
-	/*
-		router := gin.Default()
-		router.GET("/ping", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
-		router.Run() // listen and serve on 0.0.0.0:8080
-	*/
+	})
+	transactions := router.Group("/transactions")
+	{
+		transactions.GET("/", getAllTransactionsHandler)
+		transactions.GET("/uncategorized", getAllUncategorizedTransactionsHandler)
+		transactions.GET("/:category", getAllTransactionsInCategoryHandler)
+		transactions.GET("/:timestamp", getAllTransactionsAfterDateTimeHandler)
+		transactions.POST("/:chargeid", postNewTransactionHandler)
+		transactions.PUT("/:chargeid", modifyTransactionHandler)
+	}
+	budget := router.Group("/budget")
+	{
+		budget.GET("/", getAllBudgetCategoriesHandler)
+		budget.POST("/:category", insertNewCategoryHandler)
+		budget.PUT("/:category", modifyCategoryHandler)
+	}
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getAllTransactionsHandler(c *gin.Context) {
+
+}
+
+func getAllUncategorizedTransactionsHandler(c *gin.Context) {
+
+}
+
+func getAllTransactionsInCategoryHandler(c *gin.Context) {
+	category := c.Param("category")
+}
+
+func getAllTransactionsAfterDateTimeHandler(c *gin.Context) {
+
+}
+
+func postNewTransactionHandler(c *gin.Context) {
+
+}
+
+func modifyTransactionHandler(c *gin.Context) {
+
+}
+
+func getAllBudgetCategoriesHandler(c *gin.Context) {
+
+}
+
+func insertNewCategoryHandler(c *gin.Context) {
+
+}
+
+func modifyCategoryHandler(c *gin.Context) {
+
 }
