92c2eb2f3dda719f378384a09ef78a763b982d7b
Philip Chang <philip.wilson.chang@gmail.com>
2019-09-26 23:41:40 -0400

    Implement FixedSpendingList component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import CategoryDisplay from './components/CategoryDisplay';
+import FixedSpendingList, { Cost } from './components/FixedSpendingList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const testList: Cost[] = [
+  {name: "Item A", amount: 200},
+  {name: "Item B", amount: 300},
+  {name: "Item C", amount: 400}
+];
+
 const App: React.FC = () => {
   return (
+    <div>
     <CategoryDisplay name="Test" limit={300} spent={200} />
+    <br />
+    <FixedSpendingList costs={testList} removeCost={(name) => console.log("REMOVE", name)} addCost={(name, amount) => console.log("ADD", name, amount)} />
+    </div>
   );
 }
 
--- web/src/components/FixedSpendingList.tsx
+++ web/src/components/FixedSpendingList.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FixedSpendingList: React.FC = () => {
-	return (<div></div>);
+import { FormControl, FormControlProps } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import { FaPlusCircle, FaTrashAlt } from "react-icons/fa";
+
+export type Cost = {
+	name: string,
+	amount: number
+};
+
+interface IFixedSpendingListProps {
+	costs: Cost[],
+	removeCost(name: string): void,
+	addCost(name: string, amount: number): void
+};
+
+const FixedSpendingList: React.FC<IFixedSpendingListProps> = ({ costs, removeCost, addCost }) => {
+	const [newName, setNewName] = useState("");
+	const [newAmount, setNewAmount] = useState(0);
+
+	return (
+		<Card style={{ width: '75%'}}>
+			<Card.Header>Fixed Monthly Costs</Card.Header>
+			<Card.Body>
+			<Table striped bordered hover>
+			  <thead>
+			    <tr>
+			      <th></th>
+			      <th>Monthly Cost</th>
+			      <th>Amount</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  	{costs.map(cost => 
+			  		(<tr key={cost.name}>
+				      <td style={{ textAlign: 'center', width: '25px' }}>
+				      	<Button variant="outline-danger" onClick={() => removeCost(cost.name)}>
+				      		<FaTrashAlt />
+				      	</Button>
+				      </td>
+				      <td>{cost.name}</td>
+				      <td>${cost.amount.toFixed(2)}</td>
+			    	</tr>)
+			  	)} 
+			  </tbody>
+			</Table>
+			<Form>
+			    <Form.Row>
+			    	<Col>
+			      	</Col>
+				    <Col>
+				        <Form.Control placeholder="Cost name" onChange={(e: React.FormEvent<FormControlProps & FormControl>) => setNewName(e.currentTarget.value as string)}/>
+				    </Col>
+				    <Col>
+				        <Form.Control placeholder="Cost amount" onChange={(e: React.FormEvent<FormControlProps & FormControl>) => setNewAmount(parseFloat(e.currentTarget.value as string))}/>
+				    </Col>
+				    <Button variant="outline-success" onClick={() => addCost(newName, newAmount)}>
+			      		<FaPlusCircle />
+			      	</Button>
+			    </Form.Row>
+			</Form>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default FixedSpendingList;
