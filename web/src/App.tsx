55d6a108aae4e89b6f10b75b9e6ab02adbb1f87a
Philip Chang <philip.wilson.chang@gmail.com>
2019-10-15 15:04:39 -0400

    Add tabs to App, organize containers into app layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,47 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import CategoryDisplay from './components/CategoryDisplay';
-import SpendingAdder from './components/SpendingAdder';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 import FixedSpendingListContainer from './containers/FixedSpendingListContainer';
 import FlexSpendingListContainer from './containers/FlexCategoryListContainer';
 import IncomeInputContainer from './containers/IncomeInputContainer';
+import SpendingAdderContainer from './containers/SpendingAdderContainer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const prismaAPI = 'http://localhost:4466';
 
-const testCategories = [
-  'Food',
-  'Groceries',
-  'Fun'
-];
-
 const App: React.FC = () => {
+  const [tabKey, setTabKey] = useState('flex-summary');
+
   return ( 
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ width: '90%' }}>
-        <FixedSpendingListContainer api={prismaAPI} />
-        <br />
-        <FlexSpendingListContainer api={prismaAPI} />
-        <br />
-        <IncomeInputContainer api={prismaAPI} />
-        <br />
-        <CategoryDisplay name="Test" limit={300} spent={200} />
-        <br />
-        <SpendingAdder 
-          categories={testCategories} 
-          selectedCategory='Fun' 
-          categoryLimit={200} 
-          categorySpent={150} 
-          amount={20.54} 
-          changeCategory={(cat) => console.log("Change category:", cat)}
-          changeAmount={(amt) => console.log("Change amount:", amt)}
-          submitFunc={() => console.log("SUBMIT")}
-        />
-      </div>
-    </div>
+    // <div style={{ display: 'flex', justifyContent: 'center' }}>
+    //   <div style={{ width: '90%' }}>
+    //     <SpendingAdderContainer api={prismaAPI} />
+    //     <br />
+    //     <FixedSpendingListContainer api={prismaAPI} />
+    //     <br />
+    //     <FlexSpendingListContainer api={prismaAPI} />
+    //     <br />
+    //     <IncomeInputContainer api={prismaAPI} />
+    //   </div>
+    // </div>
+    <Tabs id="app" activeKey={tabKey} onSelect={(k: string) => setTabKey(k)}>
+      <Tab id="flex-summary" eventKey="flex-summary" title="Summary">
+      </Tab>
+      <Tab id="flex-add" eventKey="flex-add" title="Add Spending">
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '90%' }}>
+            <br />
+            <SpendingAdderContainer api={prismaAPI} />
+          </div>
+        </div>
+      </Tab>
+      <Tab id="budget" eventKey="budget" title="Modify Budget">
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '90%' }}>
+            <br />
+            <IncomeInputContainer api={prismaAPI} />
+            <br />
+            <FixedSpendingListContainer api={prismaAPI} />
+            <br />
+            <FlexSpendingListContainer api={prismaAPI} />
+           </div>
+        </div>
+      </Tab>
+    </Tabs>
   );
 }
 
