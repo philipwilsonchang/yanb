6e5635fe1a5b40befaa8bdf479d0f3b9c7deb3fc
Philip Chang <philip.wilson.chang@gmail.com>
2020-01-28 15:36:02 -0500

    Change backend schema to combine all CostCategory types, add RollingCostCategory resolvers

--- prisma-server/src/index.js
+++ prisma-server/src/index.js
@@ -1,10 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 
+const monthDiff = (dateFrom, dateTo) => {
+  const result = dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear())) + 1;
+  return result;
+}
+
 const resolvers = {
   Query: {
     getAllFlexCategoriesBetweenTimes: async (_, args, context, info) => {
-      const flexCategories = await context.prisma.query.flexCostCategories();
+      const flexCategories = await context.prisma.query.costCategories({
+        where: {
+          type: "FLEX"
+        }
+      });
       const spentFlexCategories = flexCategories.map( async (category) => {
         const costsPerCategory = await context.prisma.query.costs({
           where: {
@@ -23,56 +32,94 @@ const resolvers = {
       return spentFlexCategories
     },
     getAllFixedCategories: (_, args, context, info) => {
-      return context.prisma.query.fixedCostCategories()
+      return context.prisma.query.costCategories({
+        where: {
+          type: "FIXED"
+        }
+      })
+    },
+    getAllRollingCategoriesBetweenTimes: async (_, args, context, info) => {
+      // todo: design totalLimit calculation
+      const rollingCats = await context.prisma.query.costCategories({
+        where: {
+          type: "ROLLING"
+        }
+      });
+
+      const today = new Date();
+      const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+      
+      const spentRollingCats = rollingCats.map( async (category) => {
+        const costsPerCategoryBeforeThisMonth = await context.prisma.query.costs({
+          where: {
+            category: {
+              id: category.id,
+            },
+            createdAt_lte: firstDay.toISOString()
+          }
+        });
+        const timedCostsPerCategory = await context.prisma.query.costs({
+          where: {
+            category: {
+              id: category.id,
+            },
+            createdAt_gte: args.timeStart,
+            createdAt_lte: args.timeEnd,
+          }
+        });
+        const totalSpentBeforeThisMonth = costsPerCategoryBeforeThisMonth.reduce((acc, cost) => {
+          return acc + cost.amount;
+        }, 0);
+        const timedSpent = timedCostsPerCategory.reduce((acc, cost) => {
+          return acc + cost.amount;
+        }, 0);
+        const monthxLimit = monthDiff(new Date(category.createdAt), new Date()) * category.monthlyLimit;
+        return { ...category, totalLimit: monthxLimit - totalSpentBeforeThisMonth, spent: timedSpent }
+      })
+      return spentRollingCats;
     },
     getMonthlyIncomes: (_, args, context, info) => {
       return context.prisma.query.monthlyIncomes()
     }
   },
+
   Mutation: {
     createCost: async (_, args, context, info) => {
       const newCost = await context.prisma.mutation.createCost({
         data: args.data,
         info,
       });
-      const costCategory = await context.prisma.query.flexCostCategory({
+      const costCategory = await context.prisma.query.costCategory({
         where: {
           id: args.data.category.connect.id
         }
       });
       return { ...newCost, category: costCategory };
     },
-    createFixedCostCategory: (_, args, context, info) => {
-      return context.prisma.mutation.createFixedCostCategory({
+    deleteCost: async (_, args, context, info) => {
+      const deletedCost = await context.prisma.mutation.deleteCost({
+        where: {
+          id: args.id,
+        },
+        info,
+      });
+      return deletedCost;
+    },
+    createCostCategory: async (_, args, context, info) => {
+      const createdCategory = await context.prisma.mutation.createCostCategory({
         data: args.data,
         info,
-      })
+      });
+      return createdCategory;
     },
-    deleteFixedCostCategory: (_, args, context, info) => {
-      return context.prisma.mutation.deleteFixedCostCategory(
-        {
-          where: {
-            id: args.id,
-          },
-          info,
-        }
-      )
-    },
-    createFlexCostCategory: (_, args, context, info) => {
-      return context.prisma.mutation.createFlexCostCategory({
-        data: args.data,
+    deleteCostCategory: async (_, args, context, info) => {
+      const deletedCategory = await context.prisma.mutation.deleteCostCategory({
+        where: {
+          id: args.id,
+        },
         info,
       })
-    },
-    deleteFlexCostCategory: (_, args, context, info) => {
-      return context.prisma.mutation.deleteFlexCostCategory(
-        {
-          where: {
-            id: args.id,
-          },
-          info,
-        }
-      )
+      return deletedCategory;
     },
     upsertMonthlyIncome: (_, args, context, info) => {
       return context.prisma.mutation.upsertMonthlyIncome({
