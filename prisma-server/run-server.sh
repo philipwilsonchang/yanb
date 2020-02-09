5f2ad6d731db5aeced019f56cfba10d9fbfb94e5
Philip Chang <philip.wilson.chang@gmail.com>
2020-02-09 16:40:36 -0500

    Update run-server.sh shell script to start GraphQL server too

--- prisma-server/run-server.sh
+++ prisma-server/run-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose up -d && echo 'Waiting for prisma services to start...' && until prisma info; do sleep 5s; done && prisma deploy && prisma playground;
+docker-compose up -d && echo 'Waiting for prisma services to start...' && until prisma info; do sleep 5s; done && prisma deploy && node src/index.js; 
